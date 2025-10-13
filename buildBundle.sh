94349dda7d59285fb9ce259291e2c03e11301463
vitali <vitali.krieger@stud.hn.de>
2025-10-13 14:45:51 +0200

    Fix: buildBundle.sh couldnt handle utf8 before.

--- buildBundle.sh
+++ buildBundle.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# support umlaute and other utf8 stuff
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+set -e
 
 BUILDER_SRC_DIR="./src/builder"
 DIST_DIR="./dist"
@@ -8,41 +12,30 @@ OUT_HTML="$DIST_DIR/storyBuilder.html"
 mkdir -p "$DIST_DIR"
 
 # Move all js to a single file
-esbuild "$BUILDER_SRC_DIR/main.js" --bundle --minify --outfile="$BUNDLE_JS"
+esbuild "$BUILDER_SRC_DIR/main.js" --bundle --minify --charset=utf8 --outfile="$BUNDLE_JS"
 
 # read css ,js and other html file contents
-CSS=$(cat "$BUILDER_SRC_DIR/style.css")
-JS=$(cat "$BUNDLE_JS")
-HTML_VIEWER=$(cat "src/storyViewer.html")
-HTML_BUILDER=$(cat "$BUILDER_SRC_DIR/storyBuilder.html")
+CSS=$(<"$BUILDER_SRC_DIR/style.css")
+JS=$(<"$BUNDLE_JS")
+HTML_VIEWER=$(<"src/storyViewer.html")
+HTML_BUILDER=$(<"$BUILDER_SRC_DIR/storyBuilder.html")
 
-# replace or insert with awk: CSS, JS and Viewer incurrentLine to HTML_BUILDER
+# inline everything
 awk -v css="$CSS" \
     -v js="$JS" \
     -v viewer="$HTML_VIEWER" '
 {
     currentLine = $0
 
-    # remove line with insertViewerTemplate.js
-    if (line ~ /<script src="insertViewerTemplate\.js"><\/script>/) {
-        next
-    }
-    # replace css
-    else if (currentLine ~ /<link rel="stylesheet" type="text\/css" href="style.css">/) {
+    if (currentLine ~ /<script src="insertViewerTemplate\.js"><\/script>/) next
+    else if (currentLine ~ /<link rel="stylesheet" type="text\/css" href="style.css">/)
         print "<style>" css "</style>"
-    }
-    # replace js
-    else if (currentLine ~ /<script src="main.js" type="module"><\/script>/) {
+    else if (currentLine ~ /<script src="main.js" type="module"><\/script>/)
         print "<script>" js "</script>"
-    }
-    # Insert viewer template to <pre> in BUILDER_HTML
-    else if (currentLine ~ /<pre id="storyViewerTemplate">/) {
+    else if (currentLine ~ /<pre id="storyViewerTemplate">/)
         print "<pre id=\"storyViewerTemplate\">" viewer "</pre>"
-    }
-    # OUTPUT ALL OTHER LINES UNCHANGED
-    else {
+    else
         print currentLine
-    }
 }
 ' <<< "$HTML_BUILDER" > "$OUT_HTML"
 
