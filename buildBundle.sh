8d4cfb8f8e0b67a55c041313a953968d79117f7a
vitali <vitali.krieger@stud.hn.de>
2025-11-05 17:39:47 +0100

    Codechange: Not use system() in buildBundle.sh

--- buildBundle.sh
+++ buildBundle.sh
@@ -4,6 +4,7 @@ export LANG=en_US.UTF-8
 set -e
 
 BUILDER_SRC_DIR="./src/builder"
+VIEWER_HTML_CONTENT=$(<"./src/storyViewer.html")
 DIST_DIR="./dist"
 BUNDLE_JS="$DIST_DIR/bundle.js"
 OUT_HTML="$DIST_DIR/storyBuilder.html"
@@ -25,26 +26,33 @@ esbuild "$BUILDER_SRC_DIR/main.js" \
 # read css ,js and other html file contents
 CSS=$(<"$BUILDER_SRC_DIR/style.css")
 JS=$(<"$BUNDLE_JS")
-HTML_BUILDER=$(<"$BUILDER_SRC_DIR/storyBuilder.html")
 
 # inline everything
-awk -v css="$CSS" \
-    -v js="$JS" '
-{
-    currentLine = $0
-
-    if (currentLine ~ /<script src="insertViewerTemplate\.js"><\/script>/) next
-    else if (currentLine ~ /<link rel="stylesheet" type="text\/css" href="style.css">/)
-        print "<style>" css "</style>"
-    else if (currentLine ~ /<script src="main.js" type="module"><\/script>/)
-        print "<script>" js "</script>"
-    else if (currentLine ~ /<pre id="storyViewerTemplate">/) {
-        print "<pre id=\"storyViewerTemplate\">"
-        # escape html:
-        system("cat src/storyViewer.html | sed \"s/&/\\&amp;/g; s/</\\&lt;/g; s/>/\\&gt;/g\"")
-    } else
-        print currentLine
+awk -v css="$CSS" -v js="$JS" -v viewer="$VIEWER_HTML_CONTENT" '
+function escape_html(str,   out, i, c) {
+    out = ""
+    for (i = 1; i <= length(str); i++) {
+        c = substr(str, i, 1)
+        if (c == "&") out = out "&amp;"
+        else if (c == "<") out = out "&lt;"
+        else if (c == ">") out = out "&gt;"
+        else out = out c
+    }
+    return out
 }
-' <<< "$HTML_BUILDER" > "$OUT_HTML"
+{
+    if ($0 ~ /<script src="insertViewerTemplate\.js"><\/script>/) next
+    else if ($0 ~ /<link rel="stylesheet" type="text\/css" href="style.css">/)
+        print "<style>" css "</style>"
+    else if ($0 ~ /<script src="main.js" type="module"><\/script>/)
+        print "<script>" js "</script>"
+    else if ($0 ~ /<pre id="storyViewerTemplate">/) {
+        print "<pre id=\"storyViewerTemplate\">"
+        n = split(viewer, lines, "\n")
+        for (i=1; i<=n; i++) print escape_html(lines[i])
+    } else
+        print
+}
+' "$BUILDER_SRC_DIR/storyBuilder.html" > "$OUT_HTML"
 
 echo "Bundle created: $OUT_HTML"
