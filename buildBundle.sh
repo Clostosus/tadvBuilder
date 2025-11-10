aa38d7f71692ffb082ab59c2c4d4232393c04b38
vitali <vitali.krieger@stud.hn.de>
2025-11-10 08:37:23 +0100

    Remove: Viewer Template in GUI
    Fix: Tree preview in GUI when Story is loaded from json.

--- buildBundle.sh
+++ buildBundle.sh
@@ -4,7 +4,6 @@ export LANG=en_US.UTF-8
 set -e
 
 BUILDER_SRC_DIR="./src/builder"
-VIEWER_HTML_CONTENT=$(<"./src/storyViewer.html")
 DIST_DIR="./dist"
 BUNDLE_JS="$DIST_DIR/bundle.js"
 OUT_HTML="$DIST_DIR/storyBuilder.html"
@@ -28,29 +27,13 @@ CSS=$(<"$BUILDER_SRC_DIR/style.css")
 JS=$(<"$BUNDLE_JS")
 
 # inline everything
-awk -v css="$CSS" -v js="$JS" -v viewer="$VIEWER_HTML_CONTENT" '
-function escape_html(str,   out, i, c) {
-    out = ""
-    for (i = 1; i <= length(str); i++) {
-        c = substr(str, i, 1)
-        if (c == "&") out = out "&amp;"
-        else if (c == "<") out = out "&lt;"
-        else if (c == ">") out = out "&gt;"
-        else out = out c
-    }
-    return out
-}
+awk -v css="$CSS" -v js="$JS" '
 {
-    if ($0 ~ /<script src="insertViewerTemplate\.js"><\/script>/) next
-    else if ($0 ~ /<link rel="stylesheet" type="text\/css" href="style.css">/)
+    if ($0 ~ /<link rel="stylesheet" type="text\/css" href="style.css">/)
         print "<style>" css "</style>"
     else if ($0 ~ /<script src="main.js" type="module"><\/script>/)
         print "<script>" js "</script>"
-    else if ($0 ~ /<pre id="storyViewerTemplate">/) {
-        print "<pre id=\"storyViewerTemplate\">"
-        n = split(viewer, lines, "\n")
-        for (i=1; i<=n; i++) print escape_html(lines[i])
-    } else
+    else
         print
 }
 ' "$BUILDER_SRC_DIR/storyBuilder.html" > "$OUT_HTML"
